3fca7b60f8518cb38f1890c589b324bea1b671e4
nntan041299 <147411974+nntan041299@users.noreply.github.com>
2026-02-25 14:46:18 +0700

    Integrate terraform with github actions (#27)
    Co-authored-by: annguyen <nntan041299@gmail.com>

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,9 +1,11 @@
 locals {
+  ssm_env_path = "/bakery/${var.env}"
+
   env_from_ssm = zipmap(
     [for n in data.aws_ssm_parameters_by_path.app_env.names : basename(n)],
     data.aws_ssm_parameters_by_path.app_env.values
   )
 
   # allow overriding/adding from var.env
-  env_all = merge(local.env_from_ssm, var.env)
+  env_docker_all = merge(local.env_from_ssm, var.env_docker)
 }
